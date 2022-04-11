57917446fcdd618e9231dc144983ae4fee3573ec
dobiukas <86017003+dobiukas@users.noreply.github.com>
2022-04-11 19:33:40 +0300

    Initial commit

---
+++ mysite/food/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import *
+
+# Register your models here.
+
+class foodAdmin(admin.ModelAdmin):
+    class Meta:
+        model=Fooditem
+    list_display=['name']
+    list_filter=['name']
+
+admin.site.register(Customer)
+admin.site.register(UserFooditem)
+admin.site.register(Category)
+admin.site.register(Fooditem,foodAdmin)
